7405b81257a077c8a5573c494cd9e2e88d6bbbdc
anthony.mart.akimad <anthony.mart.akimad@gmail.com>
2022-02-04 07:34:50 -0400

    config env vars

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
+require('dotenv').config();
+
 // ----------------------------------------------------------------------
 
 ReactDOM.render(
